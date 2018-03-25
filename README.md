a85b105751284f8bf460ff37026ef0d262a8864d
Jeff Hajewski <jeff.hajewski@gmail.com>
2018-03-25 09:19:47 -0500

    Initial commit with README and solution to day 1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Advent of Code
+Solutions to the 2017 Advent of Code challenges (albeit, completed in 2018)
---
+++ src/day-1/day1.cpp
@@ -0,0 +1,37 @@
+#include <cstdlib>
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+int matching_sum(const std::string& digits) {
+  size_t n = digits.size();
+  int sum {0};
+  for (size_t i = 0; i < n; ++i) { 
+    if (i < n - 1 && (digits[i] == digits[i+1])) sum += digits[i] - '0';
+    else if (i == n - 1 && (digits[n-1] == digits[0])) sum += digits[n-1] - '0';
+  }
+  return sum;
+}
+
+void run_tests() {
+  std::unordered_map<std::string, int> tests = {{"1111", 4},
+    {"1234", 0}, {"91212129", 9}, {"1122", 3}};
+  for (const auto& p : tests) {
+    const auto expected = p.second;
+    const auto actual = matching_sum(p.first);
+    if (actual == expected) {
+      std::cout << "[PASS]: " << p.first << '\n';
+    } else {
+      std::cout << "[FAIL]: " << p.first << '\n';
+      std::cout << "\tExpected: " << expected << '\n'
+                << "\tGot: " << actual << '\n';
+    }
+  }
+}
+
+int main(int argc, char* argv[]) {
+  std::string input;
+  std::cin >> input;
+  std::cout << "Answer: " << matching_sum(input) << '\n';
+  return 0;
+}
